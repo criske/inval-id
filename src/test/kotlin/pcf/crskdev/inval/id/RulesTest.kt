cc39822a92ebb91a09284313587d4e611b312fb4
cristianpela <cristypela05@gmail.com>
2021-02-16 12:24:52 +0200

    #9 NotBlank rule.

--- src/test/kotlin/pcf/crskdev/inval/id/RulesTest.kt
+++ src/test/kotlin/pcf/crskdev/inval/id/RulesTest.kt
@@ -27,22 +27,31 @@ package pcf.crskdev.inval.id
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import pcf.crskdev.inval.id.Rules.NotBlank
+import pcf.crskdev.inval.id.Rules.NotEmpty
 
 internal class RulesTest : DescribeSpec({
 
-    context("Not Empty Tests") {
+    describe("Not Empty Tests") {
 
         it("should apply rule") {
-            (Rules.NotEmpty<String>() validates "" withId 1)().isFailure shouldBe true
-            (Rules.NotEmpty<Array<Any>>() validates emptyArray() withId 1)().isFailure shouldBe true
-            (Rules.NotEmpty<Collection<Any>>() validates emptyList() withId 1)().isFailure shouldBe true
-            (Rules.NotEmpty<Map<Any, Any>>() validates emptyMap() withId 1)().isFailure shouldBe true
+            (NotEmpty<String>() validates "" withId 1)().isFailure shouldBe true
+            (NotEmpty<Array<Any>>() validates emptyArray() withId 1)().isFailure shouldBe true
+            (NotEmpty<Collection<Any>>() validates emptyList() withId 1)().isFailure shouldBe true
+            (NotEmpty<Map<Any, Any>>() validates emptyMap() withId 1)().isFailure shouldBe true
         }
 
         it("should throw type is not allowed") {
             shouldThrow<IllegalArgumentException> {
-                (Rules.NotEmpty<Int>() validates 1 withId 1)()
+                (NotEmpty<Int>() validates 1 withId 1)()
             }
+        }
+    }
+
+    describe("Not Blank Tests") {
+
+        it("should apply rule") {
+            (NotBlank() validates "   " withId 1)().isFailure shouldBe true
         }
     }
 })
